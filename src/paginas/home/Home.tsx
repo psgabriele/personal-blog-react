5db43e1d29262d3b361196aec86896072051021c
Gabriele Sá <gabrielepinheirosa@gmail.com>
2022-06-24 19:47:34 -0300

    Primeiro componente no React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://www.triscele.com.br/wp-content/uploads/2017/01/desenvolvimento-web.jpg" alt="Ferramentas de desenvolvimento web" className="img"/>
+        </>
+    );
+}
+
+export default Home;
