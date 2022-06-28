a9ab0b7f70ae7f07bac75a5bbbc72bc29fefecc1
Gabriele Sá <gabrielepinheirosa@gmail.com>
2022-06-28 11:21:12 -0300

    Mais alterações em Navbar

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import {AppBar, Toolbar} from '@material-ui/core';
+import {Box} from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
