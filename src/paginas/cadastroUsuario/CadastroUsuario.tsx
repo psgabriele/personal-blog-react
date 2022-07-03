4942e28ec689a301e8982f3b75b64d209b25cade
Gabriele Sá <gabrielepinheirosa@gmail.com>
2022-07-03 20:09:16 -0300

    Comunicação com o back-end - Cadastro

--- src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -1,10 +1,62 @@
-import react from 'react';
+import react, {useState, useEffect, ChangeEvent} from 'react';
+import { useNavigate } from 'react-router-dom';
+import User from '../../models/User';
 import {Grid, TextField, Typography, Button} from '@material-ui/core';
 import {Box} from '@mui/material';
 import {Link} from 'react-router-dom';
 import './CadastroUsuario.css';
+import { cadastroUsuario } from '../../services/Service';
 
 function CadastrarUsuario() {
+
+    let navigate = useNavigate();
+    const [confirmarSenha,setConfirmarSenha] = useState<String>("")
+    const [user, setUser] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: ''
+        })
+
+    const [userResult, setUserResult] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: ''
+        })
+
+    useEffect(() => {
+        if (userResult.id != 0) {
+            navigate("/login")
+        }
+    }, [userResult])
+
+
+    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>){
+        setConfirmarSenha(e.target.value)
+    }
+
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        if(confirmarSenha == user.senha){
+        cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult)
+        alert('Usuario cadastrado com sucesso!')
+        }else{
+            alert('Dados inconsistentes. Favor verificar as informações de cadastro.')
+        }
+    }
+
     return(
         <>
         <Grid container direction='row' justifyContent='center' alignItems='center'>
@@ -13,12 +65,12 @@ function CadastrarUsuario() {
             </Grid>
             <Grid item xs={6} alignItems='center'>
                 <Box paddingX={10}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className="text-title">Cadastrar</Typography>
-                        <TextField id="nome" label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
-                        <TextField id="usuario" label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id="senha" label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
-                        <TextField id="confirmarSenha" label='confirmar senha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth />
+                        <TextField value={user.nome} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="nome" label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
+                        <TextField value={user.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="usuario" label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={user.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="senha" label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={confirmarSenha} onChange={(e:ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id="confirmarSenha" label='confirmar senha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth />
                         <Box marginTop={3} textAlign='center' className='buttons'>
                             <Button className="btnCadastrar" type='submit' variant='contained'>Cadastrar</Button>
                             <Link to='/login'>
